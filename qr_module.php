935c5f95457665e10d6457d0de8df6c51d9ee2a4
jsaleix <julian.saleix@gmail.com>
2021-07-19 16:07:47 +0200

    Display configuration form added / color picker added in Form

--- qr_module.php
+++ qr_module.php
@@ -27,8 +27,22 @@ class Qr_Module extends Module{
         }
     }
 
+    private function setConfigurationValues(){
+        if (!Configuration::updateValue('QR_MODULE_NAME', 'TEST') || !Configuration::updateValue('QR_MODULE_COLOR', '0-0-0') ){
+            return false;
+        }
+        return true;
+    }
+
+    private function removeConfigurationValues(){
+        if (!Configuration::deleteByName('QR_MODULE_NAME') || !Configuration::deleteByName('QR_MODULE_COLOR') ){
+            return false;
+        }
+        return true;
+    }
+
     public function install(){
-        if( !parent::install() || !$this->registerHook('leftColumn') || !$this->registerHook('header') || !Configuration::updateValue('QR_MODULE_NAME', 'TEST') ){
+        if( !parent::install() || !$this->registerHook('leftColumn') || !$this->registerHook('header') || !$this->setConfigurationValues() ){
             return false;
         }else{
             return true;
@@ -36,10 +50,86 @@ class Qr_Module extends Module{
     }
 
     public function uninstall(){
-        if( !parent::uninstall() || !Configuration::deleteByName('QR_MODULE_NAME') ){
+        if( !parent::uninstall() || !$this->removeConfigurationValues() ){
             return false;
         }else{
             return true;
         }
     }
+
+    public function getContent()
+	{
+		//gestion des données du formulaire
+
+		$output = null;
+
+		//Vérifier si le formulaire a été envoyé
+		if (Tools::isSubmit('btnSubmit')) {
+			//récupere la valeur du champ txt
+			$pageName = strval(Tools::getValue('QR_MODULE_NAME'));
+
+			//Vérifie qu'il n'est pas vide
+			if (!$pageName||empty($pageName))
+			{
+				//Si oui, affiche une erreur
+				$output .= $this->displayError($this->l('Valeur invalide'));
+			} else {
+				Configuration::updateValue('QR_MODULE_NAME', $pageName);
+				//notif succes
+				$output .= $this->displayConfirmation($this->l('Valeur mise à jour'));
+			}
+		}
+
+		return $output.$this->displayForm();
+	}
+
+    public function displayForm() 
+	{
+		//Affichage du formulaire
+
+		$form = array(
+			'form' => array(
+				'legend' => array(
+					'title' => $this->l('Settings'),
+				),
+				'input' => array(
+					array(
+						'type' => 'text',
+						'label' => $this->l('Configuration value'),
+						'name' => 'QR_MODULE_NAME',
+						'size' => 20,
+						'required' => true
+                    ),
+                    array(
+                        'type' => 'color',
+                        'label' => $this->l('Color'),
+                        'name' => 'TK_THEME_BG_COLOR'
+                        )						
+                    
+				),
+				'submit' => array(
+					'title' => $this->l('Save'),
+					'name' => 'btnSubmit'
+				)
+			),
+		);
+
+		$helper = new HelperForm();
+
+		$helper->module = $this;
+		$helper->name_controller = $this->name;
+		$helper->token = Tools::getAdminTokenLite('AdminModules');
+		$helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+
+		//Langue
+		//$defaultLang = (int)Configuration::get('PS_LANG_DEFAULT');
+		//$helper->default_form_language = $defaultLang;
+
+		//charge la valeur de AG_MODULE_NAME
+		$helper->fields_value['AG_MODULE_NAME'] = Configuration::get('AG_MODULE_NAME');
+
+		return $helper->generateForm(array($form));
+	}
+
+
 }
